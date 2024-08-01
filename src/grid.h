709762431fcf77cb207cd4b045291e61eb12ad9a
uri singer <urisig2007@gmail.com>
2024-08-02 00:03:48 +0300

    looks really well now, next is cleanup and better looking renders

--- src/grid.h
+++ src/grid.h
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include <raylib.h>
 
 typedef struct Grid{
   size_t size_x;
@@ -7,20 +8,24 @@ typedef struct Grid{
   float* new_v;
   float* new_u;
 
-  float* v;
-  float* u;
-  float* p;
+  float* y_velo;
+  float* x_velo;
+  float* pressure;
+  float* s;
+
+
+  float* old_smoke;
+  float* smoke;
 
   float density;
   float spacing;
 
-  float* generators;
+  float frame;
+
 }Grid;
 
-Grid grid_new_uniform(size_t size_x, size_t size_y,float value, float density);
-Grid grid_new_random(size_t size_x, size_t size_y, float min, float max, float density);
-
-void render_grid(const Grid* grid, int screen_width, int screen_height);
+Grid grid_new_uniform(size_t size_x, size_t size_y,float value, float density, float height);
+Grid grid_new_random(size_t size_x, size_t size_y, float min, float max, float density, float height);
 
 float get_s(const Grid* grid, int i, int j);
 
@@ -28,3 +33,6 @@ void make_incompressible(Grid* grid, float dt);
 void add_gravity(Grid* grid, float dt);
 void advent_velocity(Grid* grid, float dt);
 void advance_grid(Grid* grid, float dt);
+
+
+void move_ball(Grid *grid, Vector2 last_pos, Vector2 next_pos, float r, float dt);
--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include "raylib.h"
-#include "grid.h"
+#include "render.h"
 #include <bits/time.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -17,35 +18,55 @@ int main(void) {
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE);
   SetTargetFPS(60);
 
-  Grid grid = grid_new_uniform(100, 100,  10.0,100.0);
+  Grid grid = grid_new_uniform(200,200, 0.0, 1.0, 200.0);
 
   clock_t begin, end;
   begin = clock();
-  while (!WindowShouldClose()) {
 
+  Vector2 ball_pos = {.x = 0.0, .y = 0.0};
+  float ball_r = 8.0;
+
+  bool mouse_pressed = false;
+  while (!WindowShouldClose()) {
     BeginDrawing();
 
     ClearBackground(RAYWHITE);
 
-    render_grid(&grid, GetRenderWidth(), GetRenderHeight()); 
+    draw_grid(&grid);
 
     EndDrawing();
 
     end = clock();
 
-    float dt = ((float)(end - begin)) / (CLOCKS_PER_SEC / 100);
-    
-    advance_grid(&grid, dt);
+    float dt = ((float)(end - begin)) / (CLOCKS_PER_SEC);
+    if (mouse_pressed) {
+      Vector2 mouse_pos = GetMousePosition();
+      Vector2 new_pos = {.x = mouse_pos.x / GetScreenWidth(),
+                         .y = mouse_pos.y / GetScreenHeight()};
 
-    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)){
-      Vector2 pos = GetMousePosition();
-      Vector2 tile_pos = {.x = (pos.x * grid.size_x) / GetScreenWidth(), .y = (pos.y * grid.size_y) / GetScreenHeight()};
-      
-      grid.u[(int)tile_pos.y * grid.size_x  + (int)tile_pos.x] = 1000.0;
+      move_ball(&grid, ball_pos, new_pos, ball_r, dt);
+
+
+      ball_pos = new_pos;
     }
 
-    printf("%f\n", dt);
-    
+    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+      Vector2 mouse_pos = GetMousePosition();
+      Vector2 new_pos = {.x = mouse_pos.x / GetScreenWidth(),
+                         .y = mouse_pos.y / GetScreenHeight()};
+
+      move_ball(&grid, new_pos, new_pos, ball_r, dt);
+
+      ball_pos = new_pos;
+      mouse_pressed = true;
+    } else if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
+      mouse_pressed = false;
+    }
+
+
+
+    advance_grid(&grid, dt);
+
     begin = end;
   }
 
